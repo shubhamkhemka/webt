06f65c8c7e446837720155fddf7862059148d0dc
shubham <shubham.khemka98@gmail.com>
2020-02-27 18:47:32 +0530

    integrated

--- a.lec-1/activity/commands/treefy.js
+++ a.lec-1/activity/commands/treefy.js
@@ -1,5 +1,31 @@
+const fs=require('fs');
+const path=require('path');
+let uniqid=require('uniqid');
+// const root=require('../commands/untreefy');
 module.exports.treefy=function(){
-    console.log("i am treefy");
+    console.log("tree completed");
     let src=arguments[0];
-    let mode=arguments[1];
+  
+    let dest=arguments[1];
+    
+    let node=require(path.join(dest,"matadata.json"));
+    
+    treefyIt(src,dest,node);
 }
+
+// console.log(root);
+function treefyIt(src,dest,node){
+   
+    if((node.isfile)==false){
+        fs.mkdirSync(path.join(dest,node.filename));
+    }
+    else{
+      fs.copyFileSync(src,path.join(dest,node.newname));
+    }
+    for(let i=0;i<node.children.length;i++){
+        treefyIt(path.join(src,node.children[i].filename),path.join(dest,node.filename),node.children[i]);
+    }
+
+  
+  }
+  
--- a.lec-1/activity/commands/untreefy.js
+++ a.lec-1/activity/commands/untreefy.js
@@ -6,32 +6,52 @@ module.exports.untreefy=function(){
     console.log("file copied");
     let src=arguments[0];
     let dest=arguments[1];
-  untreefy(src,dest);
+  
+  let root={};
+  untreefy(src,dest,root);
+  console.log(root);
+module.exports=root;
+let data = JSON.stringify(root);
+
+fs.writeFile(dest+"\\"+"matadata.json", data, (err) => 
+{
+    if (err) throw err;
+    console.log('Data written to file');
+});
+
+}
+function untreefy(src,dest,node){
+
+    let a=fs.lstatSync(src).isDirectory();
+    if(!a){
+      let uniqname=uniqid();
+      node.isfile=true;
+      node.children=[];
+      node.filename=path.basename(src);
+      node.newname=path.basename(src)+uniqname;
+      fs.copyFileSync(src,path.join(dest,uniqname));
+      
+
+    }
+    else{
+       
+        const f=fs.readdirSync(src);
+      node.isfile=false;
+      node.filename=path.basename(src);
+      node.children=[];
+
+      for(let i=0;i<f.length;i++){
+          let childeObj={};
+          let childPath=path.join(src,f[i]);
+          untreefy(childPath,dest,childeObj);
+          node.children.push(childeObj);
+
+      }
+     
+
+    }
+
 
 }
 
-function untreefy(src,dest){
-
-
-
-let a=fs.lstatSync(src).isDirectory();
-
-if(!a){
-   
-    let uniqname =uniqid();
-    let f=path.basename(src);
-
-    fs.copyFileSync(src,path.join(dest,f+uniqname));
-
- 
-    
-}
-else{
-    const f=fs.readdirSync(src);
-
- for(let i=0;i<f.length;i++){
-     untreefy(src+'\\'+f[i],dest);
- }
-}
-}
 
